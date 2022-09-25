25c686bb14c15174397bc1b6030aec9436a059bc
mruslana8 <ruslana869@gmail.com>
2022-09-25 21:28:53 +0200

    tested is login page

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from ../locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_URL).get_attribute("href")
+        assert "login2" in login_link, "Wrong link to login page "
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form not found"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM), "Registration form not found"
